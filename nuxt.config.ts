7bba1de8d7b0da1e8ba7f6cab0751a9312e46003
Artemiy Kvyatkovskiy <artemy.kvyatkovsky@avangard-mb.ru>
2025-08-08 21:59:41 +0300

    nuxt config + package.json change urls

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,7 +7,7 @@ export default defineNuxtConfig({
   app: {
     pageTransition: { name: 'page', mode: 'out-in' },
     layoutTransition: true,
-    baseURL: '/marioyan23.github.io', // Replace with your repository name,
+    baseURL: '/', // Replace with your repository name,
     buildAssetsDir: 'assets',
   },
   ssr: true,
